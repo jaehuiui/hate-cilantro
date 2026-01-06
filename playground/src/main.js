a4ecf47eb6540dde90058109ec4c1322fdc19fad
jaehuiui <ljh9081@kaist.ac.kr>
2026-01-06 17:36:04 +0900

    [Day3] feat: add state with simple diff & patch system

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,29 +1,53 @@
-import { render } from '@kongnamul/core'
+import { render, debug } from '@kongnamul/core'
 
-const schema = {
-  type: 'div',
-  props: {
-    id: 'container',
-  },
-  children: [
-    {
-      type: 'h1',
-      props: {
-        id: 'title',
-      },
-      children: ['콩나물 좋아하세요?'],
+debug()
+
+let state = {
+  like: 0,
+}
+
+// state -> schema 생성
+function createSchema(state) {
+  return {
+    type: 'div',
+    props: {
+      id: 'container',
     },
-    {
-      type: 'button',
-      props: {
-        onClick: () => {
-          console.log('클릭 이벤트입니다.')
+    children: [
+      {
+        type: 'h1',
+        props: {
+          id: 'title',
         },
+        children: [`콩나물 좋아하는 사람: ${state.like}명`],
       },
-      children: ['콩나물 국밥 좋아하는 분들만 눌러주세요.'],
-    },
-  ],
+      {
+        type: 'button',
+        props: {
+          onClick: () => {
+            updateState(state => {
+              state.like += 1
+            })
+          },
+        },
+        children: ['콩나물 국밥 좋아하는 분들만 눌러주세요.'],
+      },
+    ],
+  }
 }
 
 const container = document.getElementById('app')
-render(schema, container)
+
+// state 업데이트 함수
+function updateState(callback) {
+  callback(state)
+  updateDOM()
+}
+
+// DOM 업데이트 동작
+function updateDOM() {
+  const nextSchema = createSchema(state)
+  render(nextSchema, container)
+}
+
+updateDOM()
